abe6bbc8428ff2f12af6c1e5ef41bac09b682585
amrit <kumaramrit38@gmail.com>
2017-04-26 11:10:54 +0530

    eventshigh_spider complete

--- scrapy_for_events/scrapy_for_events/spiders/eventshigh_spider.py
+++ scrapy_for_events/scrapy_for_events/spiders/eventshigh_spider.py
@@ -15,6 +15,9 @@ from scrapy.http import HtmlResponse
 from scrapy.spiders.crawl import CrawlSpider, Rule
 from scrapy.spiders.feed import XMLFeedSpider, CSVFeedSpider
 from scrapy.spiders.sitemap import SitemapSpider
+from scrapy.loader.processors import MapCompose
+import re
+
 
 class QuotesSpider(scrapy.Spider):
     name = "eventshigh"
@@ -27,23 +30,78 @@ class QuotesSpider(scrapy.Spider):
             'https://www.eventshigh.com/city/Mumbai',
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_duration)
 
-    def parse(self, response):
-        var3=response.css('a::attr(href)').extract()
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!",response.url)
-        print("@@@@@@@@@@@@@@@@@@@@@@@@@@",var3)
+    # def parse(self, response):
+    #     print("111111111111111111",response.url)
+    #     var4=''
+    #     if 'Chennai' in response.url:
+    #         var4=response.xpath('//ul[@id="myTab"]//a/@href').extract()
+    #     else:
+    #         var1=response.css('div.explore-tab')
+    #         var4=var1.css('a::attr(href)').extract()
+    #
+    #     for i in var4:
+    #         url=response.url + i
+    #         print("33333333333333333",url)
+    #         print("4444444444444444",scrapy.Request(url=response.url,callback=self.parse_duration))
+    #         yield scrapy.Request(url=url,callback=self.parse_duration)
 
-        page = response.url.split("/")[-2]
+
+    def parse_duration(self, response):
+        var1=response.css('div.margin-top15')
+        var3=var1.css('a::attr(href)').extract()
+        filename = '%s__%s__%s.txt' % ('eventshigh', 'city', 'event1')
+        with open(filename, 'a') as f:
+            f.write(',')
+            f.write(str(var3))
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!",len(var3),"urllll",response.url)
+
+        # page = response.url.split("/")[-2]
+        # list1=[]
+        filename = '%s__%s__%s.txt' % ('eventshigh', 'url', 'event2')
+
+
         for i in var3:
+            a=['http','#']
+            # slicee=''
+            if not any(x in i for x in a):
+                slicee='https://www.eventshigh.com'+i
 
-            if "https" in i:
-                print("$$$$$$$$$$$$$$$$$",i.split())
+                with open(filename, 'a') as f:
+                    f.write(',')
+                    f.write(str(slicee))
 
-            slicee='https://www.eventshigh.com'+i
-            print("##########################",slicee)
+            # print("##########################",slicee)
 
-        # filename = 'quotes-%s.html' % page
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s' % filename)
+                yield scrapy.Request(url=slicee,callback=self.parse_price)
+
+
+    def parse_price(self,response):
+
+        filename = '%s__%s__%s.txt' % ('eventshigh', 'price', 'data3')
+
+        var=response.css('div.ecbox-polaroid')
+        for i in var:
+
+            event_name=i.css('h3::text').extract()
+            price=(i.css('div.browse-price-wrp::text').extract())
+            # print("&&&&&&&&&&&&&&&&&&&&&",price)
+
+            with open(filename, 'a') as f:
+
+                f.write('|||')
+                f.write(str(event_name))
+                f.write(':')
+                string=price[0]
+                string=string.rstrip('\n')
+                print("11111111111111111111111", string,"222222222222222222222",response.url,"333333333333333333333333",event_name)
+
+                f.write(string)
+
+        # price=var.css('div.browse-price-wrp::text').re(r'Rs*')
+        # print("****************************",price)
+        yield scrapy.Request(url=response.url)
+
+
+
